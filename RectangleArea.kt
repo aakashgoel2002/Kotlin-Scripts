4ea717af83a5b23d761d7aa07fee1ae64a287ae5
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-20 23:32:08 +0530

    Create RectangleArea.kt

---
+++ RectangleArea.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+fun main() {
+    val inp=Scanner(System.`in`)
+    print("Enter Length: ")
+    var length:Int=inp.nextInt()
+    print("Enter Breadth: ")
+    var breadth:Int=inp.nextInt()
+    println("Area of Rectangle: "+(length*breadth))
+}
