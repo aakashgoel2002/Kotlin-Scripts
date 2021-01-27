2d28a7e9700b4ff8dcf4b2597a9d6515eefcd6a2
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-27 20:12:53 +0530

    Update Armstrong.kt

--- Armstrong.kt
+++ Armstrong.kt
@@ -1,1 +1,16 @@
-
+import java.util.Scanner
+fun main(args: Array<String>){
+    val inp=Scanner(System.`in`)
+    print("Enter the Number to be checked: ")
+    val number:Int=inp.nextInt()
+    var no=number;var n=0;var sum=0
+    while(no!=0){
+    n=no%10
+    sum+=(n*n*n)
+    no/=10
+    }
+    if(sum==number)
+    println("$number is Armstrong")
+    else
+    println("$number is not Armstrong")
+    }
