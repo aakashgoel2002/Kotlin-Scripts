dc64a151e88e16dce618937d67c463ed6e5cb69b
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-27 19:55:13 +0530

    Update CylinderVolume.kt

--- CylinderVolume.kt
+++ CylinderVolume.kt
@@ -5,5 +5,5 @@ fun main(args: Array<String>) {
     var radius:Double=inp.nextDouble()
     print("Enter the Height: ")
     var height:Double=inp.nextDouble()
-    println("Volume of Cylinder with radius $radius and height $height is ${(22.00/7.00)*radius*radius*height}")
+    println("Volume of Cylinder with Radius $radius and Height $height is ${(22.00/7.00)*radius*radius*height}")
 }
