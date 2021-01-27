534346fac2625fbfc137cfc95324c2da20b3dec6
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-27 21:25:16 +0530

    Update PerfectSquare.kt

--- PerfectSquare.kt
+++ PerfectSquare.kt
@@ -1,1 +1,14 @@
-
+import java.util.Scanner
+import kotlin.math.*
+fun main(args: Array<String>){
+    val inp=Scanner(System.`in`)
+    print("Enter the Number to be checked: ")
+    var number:Double=inp.nextDouble()
+    print("$number")
+    number=sqrt(number)
+    var i:Double=floor(number)
+    if(i==number)
+    println(" is Perfect Square")
+    else
+    println(" is not Perfect Square")
+    }
