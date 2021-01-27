703efc90285d8d37c7e8f12e1dc642a96912958b
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-27 19:53:48 +0530

    Update CylinderVolume.kt

--- CylinderVolume.kt
+++ CylinderVolume.kt
@@ -1,1 +1,10 @@
 
+import java.util.Scanner
+fun main(args: Array<String>) {
+    val inp=Scanner(System.`in`)
+    print("Enter the radius: ")
+    var radius:Double=inp.nextDouble()
+    print("Enter the height: ")
+    var height:Double=inp.nextDouble()
+    println("Volume of Cylinder with radius $radius and height $height is ${(22.00/7.00)*radius*radius*height}")
+}
