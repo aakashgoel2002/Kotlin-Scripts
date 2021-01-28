459fb82285e9c4355113e6e505d98c611cab9eba
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-28 12:20:31 +0530

    Update MultiplicationTable.kt

--- MultiplicationTable.kt
+++ MultiplicationTable.kt
@@ -1,7 +1,7 @@
 import java.util.Scanner
 fun main(args: Array<String>){
     val inp=Scanner(System.`in`)
-    print("Enter the Number of which the table is to be Printed: ")
+    print("Enter a Number to print its table: ")
     val number:Int=inp.nextInt()
     for(i in 1..10){
     println("$number * $i = ${number*i}")
