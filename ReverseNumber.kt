3b5a96603c7c8a7a449abce823a4d886e0b22500
Aakash Goel <aakashgoel2002@gmail.com>
2021-01-28 15:35:16 +0530

    Update ReverseNumber.kt

--- ReverseNumber.kt
+++ ReverseNumber.kt
@@ -1,1 +1,16 @@
 
+import java.util.Scanner
+fun main(args: Array<String>){
+    val inp=Scanner(System.`in`)
+    print("Enter a Number with Multiple Digits")
+    val number:Int=inp.nextInt()
+    var no=number
+    var n=0
+    var rev=0
+    while(no!=0){
+    n=no%10
+    no/=10
+    rev=rev*10+n
+    }
+    println("Reverse of $number is $rev")
+}
